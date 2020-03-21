f8e6170b10f8c898e5df3e401b8b1d8ef8768b34
krisswiss <chrisswiatek@hotmail.com>
2020-03-21 15:36:32 +0000

    basic html structure + google map initialized

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
+// to initialize map
 var map;
-      function initMap() {
+      
+function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 54.8, lng: -4.6},
+          zoom: 2
         });
       }
+
+
+
+
+/* 2nd autocomplete 
+
+to autocomplete Search field
+var autocompleteSearchField;
+var places;
+
+autocompleteSearchField = new google.maps.places.autocompleteSearchField((
+    document.getElementById('enterCity')), {
+        types: ['(cities)'],
+    }
+); 
+places = new google.maps.places.PlacesService(map);
+*/
+
+
+/* youtube tutorial autocomplete cities using google places api - 1st approach 
+function enterCityAutocomplete(){
+    var input = document.getElementById('enterCity');
+    var autocomplete = new.google.maps.places.Autocomplete(input);
+} */
