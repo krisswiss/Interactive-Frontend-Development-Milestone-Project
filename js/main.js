cfc4102aa1923c4c15cdf2a27781593402e90621
krisswiss <chrisswiatek@hotmail.com>
2020-03-29 17:04:03 +0000

    find me function assosiated with category selection

--- js/main.js
+++ js/main.js
@@ -419,31 +419,44 @@
 
 
         // Function FIND ME to locate user
-        function findMe() {
-            if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
+function findMe() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             };
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+            map.panTo(pos);
+            map.setZoom(13);
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-        }
+            if ($("#artGallery").is(':checked')) {
+              searchArtGallery();
+            } else if ($("#movieTheatre").is(':checked')) {
+              searchMovieTheatre();
+            } else if ($("#museum").is(':checked')) {
+              searchMuseum();
+            } else if ($("#library").is(':checked')) {
+              searchLibrary();
+            }
+
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
